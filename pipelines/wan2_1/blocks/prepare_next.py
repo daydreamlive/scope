627ab13d9460d087435fb01a64eed5dfd3c26bb1
Yondon Fu <yondon.fu@gmail.com>
2025-11-12 19:05:18 +0000

    Add PrepareNext block + fix Krea test script
    Signed-off-by: Yondon Fu <yondon.fu@gmail.com>

---
+++ pipelines/wan2_1/blocks/prepare_next.py
@@ -0,0 +1,55 @@
+from typing import Any
+
+import torch
+from diffusers.modular_pipelines import (
+    ModularPipelineBlocks,
+    PipelineState,
+)
+from diffusers.modular_pipelines.modular_pipeline_utils import (
+    ComponentSpec,
+    ConfigSpec,
+    InputParam,
+    OutputParam,
+)
+
+
+class PrepareNextBlock(ModularPipelineBlocks):
+    @property
+    def expected_components(self) -> list[ComponentSpec]:
+        return [
+            ComponentSpec("generator", torch.nn.Module),
+        ]
+
+    @property
+    def expected_configs(self) -> list[ConfigSpec]:
+        return [
+            ConfigSpec("num_frame_per_block", 3),
+        ]
+
+    @property
+    def description(self) -> str:
+        return "Prepare Next block updates state for the next latent block after the current latent block is complete"
+
+    @property
+    def inputs(self) -> list[InputParam]:
+        return [
+            InputParam(
+                "current_start_frame",
+                required=True,
+                type_hint=int,
+                description="Current starting frame index of current block",
+            ),
+        ]
+
+    @property
+    def intermediate_outputs(self) -> list[OutputParam]:
+        return []
+
+    @torch.no_grad()
+    def __call__(self, components, state: PipelineState) -> tuple[Any, PipelineState]:
+        block_state = self.get_block_state(state)
+
+        block_state.current_start_frame += components.config.num_frame_per_block
+
+        self.set_block_state(state, block_state)
+        return components, state
