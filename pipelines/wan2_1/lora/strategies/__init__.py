968791466373944d4224aef1601d370ce0a5b117
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-11-17 12:35:02 -0500

    refactor: consistent naming for lora strats
    Signed-off-by: RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>

--- pipelines/wan2_1/lora/strategies/__init__.py
+++ pipelines/wan2_1/lora/strategies/__init__.py
@@ -4,12 +4,12 @@ This package contains the individual strategy implementations for different
 LoRA merge modes.
 """
 
-from pipelines.wan2_1.lora.strategies.peft_lora import PeftLoRAManager
+from pipelines.wan2_1.lora.strategies.peft_lora import PeftLoRAStrategy
 from pipelines.wan2_1.lora.strategies.permanent_merge_lora import (
-    PermanentMergeLoRAManager,
+    PermanentMergeLoRAStrategy,
 )
 
 __all__ = [
-    "PermanentMergeLoRAManager",
-    "PeftLoRAManager",
+    "PermanentMergeLoRAStrategy",
+    "PeftLoRAStrategy",
 ]
