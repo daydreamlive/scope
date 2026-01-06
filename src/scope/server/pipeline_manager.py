60e9e895ce450af6987916b824c186bce195fbf3
BuffMcBigHuge <marco@bymar.co>
2026-01-05 21:04:20 -0500

    Fix to vae testing.
    Signed-off-by: BuffMcBigHuge <marco@bymar.co>

--- src/scope/server/pipeline_manager.py
+++ src/scope/server/pipeline_manager.py
@@ -503,9 +503,6 @@ class PipelineManager:
                     "tokenizer_path": str(
                         get_model_file_path("Wan2.1-T2V-1.3B/google/umt5-xxl")
                     ),
-                    "vae_path": str(
-                        get_model_file_path("Wan2.1-T2V-1.3B/Wan2.1_VAE.pth")
-                    ),
                     "vace_path": str(
                         get_model_file_path(
                             "WanVideo_comfy/Wan2_1-VACE_module_14B_bf16.safetensors"
