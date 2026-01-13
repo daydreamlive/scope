8926d915fc9d59c2b35a4abb0c1fb1b6b6be9a1d
Yondon Fu <yondon.fu@gmail.com>
2026-01-13 15:14:38 -0500

    Auto patch torch static cuda launcher
    Fixes unsigned long issue on Windows when using torch.compile
    
    Signed-off-by: Yondon Fu <yondon.fu@gmail.com>

--- src/scope/core/patches/_startup.py
+++ src/scope/core/patches/_startup.py
@@ -10,8 +10,10 @@ import sys
 if sys.platform == "win32":
     try:
         from .cudnn import patch_torch_cudnn
+        from .static_cuda_launcher import patch_torch_static_cuda_launcher
 
         patch_torch_cudnn(silent=True)
+        patch_torch_static_cuda_launcher(silent=True)
     except Exception:
         # Never crash Python startup - fail silently
         pass
